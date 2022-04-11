2024fefb837c50bd3bc8487c5ffe075c68c4bdd0
Mihai <Mihail.Coscoden@icl-group.com>
2022-04-11 18:47:13 +0300

    Add entities and define dbContext

---
+++ EF_Core_Quering/EF_Core_Quering/BlogDbContext.cs
@@ -0,0 +1,27 @@
+﻿using EF_Core_Quering.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_Quering
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("server=.;database=BlogDB;trusted_connection=true;");
+        }
+    }
+}
