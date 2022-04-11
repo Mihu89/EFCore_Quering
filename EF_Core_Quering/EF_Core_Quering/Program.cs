407ab07322e0aabdf5e4be3d7fe51fd10b05e4c6
Mihai <Mihail.Coscoden@icl-group.com>
2022-04-11 20:27:01 +0300

    Quering posts example

--- EF_Core_Quering/EF_Core_Quering/Program.cs
+++ EF_Core_Quering/EF_Core_Quering/Program.cs
@@ -21,13 +21,41 @@ namespace EF_Core_Quering
             // extract all posts that contains C#
             var db = new BlogDbContext();
 
+            db.Posts.Add(new Post() { Title = "Fresh", Content = " Fresh Post C#", Blog = db.Blogs.FirstOrDefault() });
             var posts = db.Posts
                         .Include(x => x.Blog)
-                        .Where(x => x.Title.Contains("C#") || x.Content.Contains("C#"));
+                        .Where(x => x.Title.Contains("C#") || x.Content.Contains("C#"))
+                        .ToArray();
+
+            // remove first element from the list
+            db.Posts.Remove(posts[0]);
+            
             foreach (var post in posts)
             {
-                Console.WriteLine($"PostID:{post.Id} {post.Title} {post.Content}, BlogID: {post.Blog.Id}");
+                Console.WriteLine($"PostID:{post.Id} {post.Title} {post.Content}, BlogID: {post.Blog.Id} PostState: {db.Entry(post).State}");
             }
+
+            var manager = db.Users.SingleOrDefault(x => x.Name=="manager");
+            Console.WriteLine($"Manager USER: {manager.Id} {manager.Name} {manager.DisplayName}");
+
+            Console.WriteLine(new string('_',20));
+            // Extract all posts that contsins EF from EF Blog.
+
+            var efBlog = db.Blogs.FirstOrDefault(x => x.Id == 2);
+            //var efPosts = db.Entry(efBlog)
+            //    .Collection(p => p.Posts)
+            //    .Query()
+            //    .Where(x => x.Title.Contains("EF"))
+            //    .ToList();
+            var efPosts = db.Posts.Include(x => x.Blog)
+                .Where(x => x.Title.Contains("EF"))
+                .ToList();
+
+            foreach (var post in efPosts)
+            {
+                Console.WriteLine($"PostID:{post.Id} {post.Title} {post.Content}, Blog_Name: {post.Blog.Name}");
+            }
+
         }
     }
 }
